f48f52f57e7fb7e8e34f1ffb133f77435f22f0d5
Jeferson Oliveira <jeferson.oliveirax@gmail.com>
2024-11-24 22:23:33 -0300

    feat: services were created to handle with business rules from each entity, leaving the controllers cleaner

---
+++ BloodBank.Application/Services/IDoacaoService.cs
@@ -0,0 +1,16 @@
+﻿using BloodBank.Application.Models;
+using BloodBank.Core.Entities;
+
+namespace BloodBank.Application.Services
+{
+    public interface IDoacaoService
+    {
+        #region Doacoes
+        ResultViewModel<List<DoacoesItemViewModel>> GetAll(string search = "");
+        ResultViewModel<DoacaoViewModel> GetById(int id);
+        ResultViewModel<int> Insert(CreateDoacaoInputModel model);
+        ResultViewModel Update(int id, UpdateDoacaoInputModel model);
+        ResultViewModel Delete(int id);
+        #endregion
+    }
+}
